0d2681f5e13aaef3a99d432418aeb4062139e441
Isaac Hunter <ikehunter5@gmail.com>
2024-10-07 00:15:51 -0400

    implement club membership in serializer

--- app/clubs/api_urls.py
+++ app/clubs/api_urls.py
@@ -5,6 +5,11 @@ from . import viewsets
 
 router = DefaultRouter()
 router.register("clubs", viewsets.ClubViewSet, basename="club")
+router.register(
+    r"clubs/(?P<club_id>.+)/members",
+    viewsets.ClubMembershipViewSet,
+    basename="club-members",
+)
 
 app_name = "api-clubs"
 
