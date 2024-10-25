c93f5facba2168edc37df0d239647412f6916c89
Marco Antonio <marcodelcas121@gmail.com>
2024-10-25 11:08:08 -0600

    adding new functions to services.py

--- app/clubs/services.py
+++ app/clubs/services.py
@@ -1,6 +1,6 @@
 from django.core import exceptions
 from django.urls import reverse
-from clubs.models import Club, ClubMembership, Event, EventAttendance
+from clubs.models import Club, ClubMembership, Event, EventAttendance,RecurringEvent
 from core.abstracts.services import ModelService
 from users.models import User
 
@@ -43,11 +43,17 @@ class ClubService(ModelService):
 
     def increase_member_coins(self, user: User, amount: int = 1):
         """Give the user more coins."""
-        pass
+        member = self._get_user_membership(user)
+        member.points += amount
 
     def decrease_member_coins(self, user: User, amount: int = 1):
         """Remove coins from the user."""
-        pass
+        member = self._get_user_membership(user)
+        if member.points < amount:
+            raise exceptions.BadRequest("Not enough coins to decrease.")
+        else:
+            member.points -= amount
+
 
     def record_member_attendance(self, user: User, event: Event):
         """Record user's attendance for event."""
@@ -67,10 +73,39 @@ class ClubService(ModelService):
         member = self._get_user_membership(user)
         return EventAttendance.objects.filter(member=member)
 
-    def create_event(self):
-        """Create new club event."""
-        pass
+    def create_event(self, name: str, **kwargs):
+        event_start = 2024-11-2
+        event_end = 2024-11-4
 
-    def create_recurring_event(self):
+        """Create new club event."""
+        if event_start >= event_end:
+            raise exceptions.BadRequest("The event start time must be before the end time.")
+
+        # Crear el evento
+        event = Event.objects.create(
+            club=self.club,
+            name=name,
+            event_start=event_start,
+            event_end=event_end,
+            **kwargs
+        )
+        return event
+
+    def create_recurring_event(self, name, club, start_date, end_date, day, event_start_time, event_end_time, location, description=None):
         """Create new recurring club event."""
-        pass
+        recurring_event = RecurringEvent.objects.create(
+            name=name,
+            club=club,
+            start_date=start_date,
+            end_date=end_date,
+            day=day,
+            event_start_time=event_start_time,
+            event_end_time=event_end_time,
+            location=location,
+            description=description,
+        )
+        
+        # Sincronizar los eventos individuales según el rango de fechas
+        recurring_event.sync_events()
+
+        return recurring_event
