8dcda09d4266e1f2ad68c398983a49dc602956a4
Isaac Hunter <ikehunter5@gmail.com>
2025-02-23 18:46:35 -0500

    implement basic poll submission recording

--- app/clubs/polls/urls.py
+++ app/clubs/polls/urls.py
@@ -6,4 +6,5 @@ app_name = "polls"
 
 urlpatterns = [
     path("poll/<int:poll_id>/", views.show_poll_view, name="poll"),
+    path("poll/<int:poll_id>/success/", views.poll_success_view, name="poll-success"),
 ]
